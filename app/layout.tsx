42e63ebf8b723901bc1e41dc7d4b92b6d0007601
besimsherifi <sherifibesim@gmail.com>
2023-10-07 16:56:55 +0200

    commit:Finished *finished the backend for the dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
+  title: 'Dashboard',
   description: 'Admin Dashboard',
 }
 
