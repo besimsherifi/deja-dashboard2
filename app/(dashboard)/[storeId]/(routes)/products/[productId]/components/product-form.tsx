f34d122632ffdb031ec08e48b6d3d7c6953e26b2
besimsherifi <sherifibesim@gmail.com>
2023-12-02 18:29:24 +0100

    * added value={(field.value ?? '')!}

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -209,7 +209,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                     {/* <Input disabled={loading} value={field.value ?? ''} placeholder="Dies gilt nur für Ringe" {...field} /> */}
                     <Input
                       disabled={loading}
-                      value={(field.value ?? '') as string}
+                      value={(field.value ?? '')!}
                       placeholder="Dies gilt nur für Ringe"
                       {...field}
                     />
