dbe2f35e61ccbf36993b277807694b5f6b052b39
besimsherifi <sherifibesim@gmail.com>
2024-07-06 14:09:39 +0200

    *fixed some prod error on login

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
- 
+export const maxDuration = 300
+
 export default function Page() {
   return <SignIn />;
 }
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
- 
+export const maxDuration = 300
+
 export default function Page() {
   return <SignUp />;
 }
