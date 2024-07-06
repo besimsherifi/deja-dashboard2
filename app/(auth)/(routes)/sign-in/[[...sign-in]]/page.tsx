a64739b3bc0aa4c1091c0d45879cca409480599f
besimsherifi <sherifibesim@gmail.com>
2024-07-06 14:12:42 +0200

    *changed maxDuration from 300 to 60

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-export const maxDuration = 300
+export const maxDuration = 60
 
 export default function Page() {
   return <SignIn />;
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
-export const maxDuration = 300
+export const maxDuration = 60
 
 export default function Page() {
   return <SignUp />;
